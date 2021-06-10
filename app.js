32ce689e2ab3e0cbc356a1d8c6ddc61a16da2ab6
Amy Oulton <amyeoulton@gmail.com>
2021-06-10 15:28:10 -0700

    project finished

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log(document.querySelector('.btn'));
+console.log(document.querySelectorAll('.btn'));
+
+console.log(document.getElementById('dont-click-me'));
+
+const clickFunc = () => {
+  document.querySelector('.secret').style.display = 'block';
+  document.getElementById('dont-click-me').innerHTML = 'Click Me Now';
+};
+
+document.getElementById('click-me').addEventListener('click', clickFunc);
+
+document.getElementById('dont-click-me').addEventListener('click', () => {
+  document.getElementById('click-me').removeEventListener('click', clickFunc);
+  document.querySelector('.secret').style.display = 'none';
+  document.getElementById('dont-click-me').innerHTML = "Don't Click Me";
+  document.getElementById('click-me').innerHTML = 'Try Me';
+});
